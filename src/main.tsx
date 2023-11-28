b4b1921df93eb69a575da96d240247aec8152c0d
A107070014 <fesu308@gmail.com>
2023-11-29 00:18:10 +0800

    [feat]setting gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import App from "./App.tsx";
 import "./index.scss";
 import { Provider } from "react-redux";
 import store from "./app/store.ts";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter basename="/ToDoList">
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
