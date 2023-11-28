8875efaf89ee79f1b8f9fd93e77f95154bf000cd
A107070014 <fesu308@gmail.com>
2023-11-29 00:26:29 +0800

    reset gh-pages basename

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/ToDoList">
+    <BrowserRouter basename="/to-do-list">
       <Provider store={store}>
         <App />
       </Provider>
