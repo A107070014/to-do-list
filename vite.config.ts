dec40eb06b2a2cc8d1771a73e773bc679af88ba8
A107070014 <fesu308@gmail.com>
2023-11-29 00:22:55 +0800

    reset gh-pages basename

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ToDoList",
+  base: "/to-do-list",
   plugins: [react()],
   css: {
     postcss: {
